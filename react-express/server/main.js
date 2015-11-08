b714bf87705b8c58fabf400b45e054dd8173d131
Oskar Niburski <oskarniburski@gmail.com>
2015-11-08 13:44:29 -0500

    Mongo connection

--- react-express/server/main.js
+++ react-express/server/main.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var ApiController = require('./routes/items.js');
+var mongoose = require('mongoose');
+
+if (process.env.HOME === '/Users/oskarniburski') {
+  console.log("connecting to local db");
+  var db = "mongodb://localhost/myapp";
+  mongoose.connect(db);
+} else {
+  console.log("connecting to prod db");
+  mongoose.connect(secrets.db);
+}
+
+mongoose.connection.on('error', function() {
+  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
+});
 
 var router = express.Router();
 
