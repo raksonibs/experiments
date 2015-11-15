317dfa3fa41843c0c9ce9dd9e0a6a74184f3bf4e
Oskar Niburski <oskarniburski@gmail.com>
2015-11-14 23:14:07 -0500

    Moving to components

--- rails-react-2/app/assets/javascripts/frontend/main.jsx
+++ rails-react-2/app/assets/javascripts/frontend/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
- 
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello</h1>
-  }
-}
 
-React.render(<Hello />, document.getElementById('hello'));
+import Hello from './components/Hello'
+
+React.render(
+  <Hello />, 
+  document.getElementById('hello')
+);
