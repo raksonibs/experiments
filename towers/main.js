57c958ba80410e7aa5e1b1d239f4daf3192bb2e3
Oskar Niburski <oskarniburski@gmail.com>
2015-11-27 17:40:09 -0500

    Trivial commits

---
+++ towers/main.js
@@ -0,0 +1,6 @@
+// need to check each pole after every move.
+// if any pole has the solution pole 1, pole 2, pole 3, in its tower box, then correct.
+// if not then no solution. 
+// need to have a recursive solver in case she gets stuck
+
+var box = document.getElementById('box')
