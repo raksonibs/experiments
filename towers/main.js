71e921b39aac64eb24b690a60a074f6e73a92433
Oskar Niburski <oskarniburski@gmail.com>
2015-11-29 19:37:14 -0500

    Better logic on rings. Now solution

--- towers/main.js
+++ towers/main.js
@@ -6,6 +6,18 @@
 var box = document.getElementById('box')
 
 
+function bottomRing($tower, $ring) {
+  var okayOrder = true
+  var lastRingNumber = $($tower.children()[0]).data('ring-number')
+  if (lastRingNumber === null || lastRingNumber === $ring.data('ring-number')) {
+    lastRingNumber = 0
+  }
+  okayOrder = lastRingNumber < $ring.data('ring-number') ? true : false
+  
+  return okayOrder
+}
+
+
 $(function() {
   var $tower1 = $(".tower-1"),
       $tower2 = $(".tower-2"),
@@ -45,18 +57,30 @@ $(function() {
   function moveRing( $item, ui ) {
     $item.fadeOut()
 
-    if (ui.position.left < $('.tower-1').position().left + $('.tower-1').width() ) {
-      console.log('moved to tower 1')
-        $('.tower-1').prepend($item)
-        $item.fadeIn()
+    if (ui.position.left < $('.tower-1').position().left + ($('.tower-1').width()/2) ) {
+      var okayOrder = bottomRing($('.tower-1'), $item)
+      if (okayOrder) {
+        console.log('moved to tower 1')
+          $('.tower-1').prepend($item)
+          // $('.tower-1 .ring').css('top', '150px');
+      }
+      $item.fadeIn()        
     } else if (ui.position.left > $('.tower-3').position().left) {
-      console.log("moved to 3")
-      $('.tower-3').prepend($item)      
-        // $item.fadeIn()
+       var okayOrder = bottomRing($('.tower-3'), $item)
+      if (okayOrder) {
+        console.log("moved to 3")
+        $('.tower-3').prepend($item)     
+      } else {
+        
+      }
+      $item.fadeIn()
     } else {
-      console.log('movsed to 2')
-      $('.tower-2').prepend($item)
-        // $item.fadeIn()
+       var okayOrder = bottomRing($('.tower-2'), $item)
+      if (okayOrder) {
+        console.log('movsed to 2')
+        $('.tower-2').prepend($item)
+        }
+        $item.fadeIn()
     }
   }
 }())
