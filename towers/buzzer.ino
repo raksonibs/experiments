7ca0d55174e668de57008a2ea45a18af6c7fe842
Oskar Niburski <oskarniburski@gmail.com>
2015-12-21 15:25:56 -0500

    Buzzer

---
+++ towers/buzzer.ino
@@ -0,0 +1,25 @@
+const int buttonPin = 2;    
+const int ledPin =  3;      
+
+
+int buttonState = 0;         
+
+void setup() {
+
+  pinMode(ledPin, OUTPUT);      
+
+  pinMode(buttonPin, INPUT);     
+}
+
+void loop(){
+
+  buttonState = digitalRead(buttonPin);
+ if (buttonState == HIGH) {     
+    digitalWrite(ledPin, HIGH);  
+  } 
+  else {
+
+    digitalWrite(ledPin, LOW); 
+  }
+}
+
