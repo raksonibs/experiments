a0d71fbfd14ef57c98593f1f12b4da3f105c4a5a
Oskar Niburski <oskarniburski@gmail.com>
2015-12-26 18:29:58 -0500

    Login componenet

--- react-firebase/src/components/App.jsx
+++ react-firebase/src/components/App.jsx
@@ -3,26 +3,37 @@ import MessageList from './MessageList.jsx'
 import ChannelList from './ChannelList.jsx'
 import * as mui from 'material-ui';
 import MessageBox from './MessageBox.jsx'
-
+import Login from './Login.jsx'
+import connectToStores from 'alt/utils/connectToStores'
+import ChatStore from "../stores/ChatStore";
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
-
 import ThemeDecorator from 'material-ui/lib/styles/theme-decorator';
 
 
 var Colors = mui.Styles.Colors
 var AppBar = mui.AppBar;
-
+@connectToStores
 class App extends React.Component {
   constructor() {
     super();
   }
 
+  static getStores() {
+    return [ChatStore]
+  }
+
+  static getPropsFromStores() {
+    return ChatStore.getState()
+  }
+
       // only one root
   render() {
-    return (
-      <div>
-        <AppBar title="Awesome Chat App" />
-        <div style={{ 
+
+    var view = <Login />
+
+    if (this.props.user) {
+      view = (
+          <div style={{ 
           display: 'flex',
           flexFlow: 'row wrap',
           maxWidth: '1200',
@@ -31,6 +42,13 @@ class App extends React.Component {
           <ChannelList />
           <MessageList />
           <MessageBox />
+        )
+    }
+
+    return (
+      <div>
+        <AppBar title="Awesome Chat App" />
+          {{view}}
         </div>
       </div>
     )
