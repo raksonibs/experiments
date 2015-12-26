2ef08cec83caf35ea82976a95f25daeaea92c696
Oskar Niburski <oskarniburski@gmail.com>
2015-12-26 18:29:47 -0500

    Change for babel 5 to use decorators as not supported in babel 6

--- react-firebase/webpack.config.js
+++ react-firebase/webpack.config.js
@@ -1,21 +1,20 @@
+
 var path = require('path');
 var webpack = require('webpack');
 
-
-
 module.exports = {
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
   entry: {
     main: [
       'webpack-dev-server/client?http://localhost:8080',
       'webpack/hot/only-dev-server',
       './src/main.js'
     ]
-  }, 
+  },
   output: {
     filename: '[name].js',
-    path: path.join(__dirname, 'publication'),
-    publicPath: '/publication'
+    path: path.join(__dirname, 'public'),
+    publicPath: '/public/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -27,7 +26,8 @@ module.exports = {
         test: /\.jsx?$/,
         include: path.join(__dirname, 'src'),
         loader: 'react-hot!babel'
-      }, {
+      },
+      {
         test: /\.scss$/,
         include: path.join(__dirname, 'src'),
         loader: 'style!css!sass'
