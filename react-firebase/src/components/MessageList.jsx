9228c3d119d95b08d4633d379f1d53adee585789
Oskar Niburski <oskarniburski@gmail.com>
2015-12-26 17:32:20 -0500

    more firebase actions

--- react-firebase/src/components/MessageList.jsx
+++ react-firebase/src/components/MessageList.jsx
@@ -11,28 +11,32 @@ class MessageList extends React.Component {
     // always first constructor with props can call super class
     super(props);
     this.state = {
-      messages: []
+      messages: {}
     }
 
     this.firebaseRef = new Firebase('https://react-stack12.firebaseio.com/messages')
-    this.firebaseRef.on('value', (dataSnapShot) => {
-      var messagesVal = dataSnapShot.val();
-      var messages = _(messagesVal)
-        .keys()
-        .map((messageKey)=> {
-          var cloned = _.clone(messagesVal[messageKey])
-          cloned.key = messageKey;
-          return cloned;
-        })
-        .value()
-      this.setState({
-        messages: messages
-      })
+    this.firebaseRef.on('child_added', (msg) => {
+      if (this.state.messages[msg.key()]) {
+        return
+      }
+
+      let msgVal = msg.val();
+
+      msgVal.key = msg.key();
+      this.state.messages[msgVal.key] = msgVal;
+      this.setState({messages: this.state.messages})
+      
+    })
+
+    this.firebaseRef.on('child_removed', (msg) => {
+      var key = msg.key()
+      delete this.state.messages[key]
+      this.setState({messages: this.state.messages})
     })
   }
 
     render() {
-      var messageNodes = this.state.messages.map((message) => {
+      var messageNodes = _.values(this.state.messages).map((message) => {
           return (
             <Message message={message.message} />
           )
