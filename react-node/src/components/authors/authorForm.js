9dc42846d1cf9c2ef57c021b6e3fc70886838338
Oskar Niburski <oskarniburski@gmail.com>
2015-12-29 00:44:57 -0500

    form controller

--- react-node/src/components/authors/authorForm.js
+++ react-node/src/components/authors/authorForm.js
@@ -6,8 +6,8 @@ var AuthorForm = React.createClass({
   render: function() {
     return (
       <form>
-        <input type="text" name="firstName" placeholder="firstname" ref="firstName" value="" /><br />
-        <input type="text"  placeholder="lastname" name="lastName" ref="firstName" value="" /><br />
+        <input type="text" name="firstName" placeholder="firstname" ref="firstName" value={this.props.author.firstName} onChange={this.props.onChange} /><br />
+        <input type="text"  placeholder="lastname" name="lastName" ref="lastName" value={this.props.author.lastName} onChange={this.props.onChange} /><br />
         <br />
         <input type="submit" value="save" />
       </form>
