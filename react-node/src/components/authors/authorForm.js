d1cd1109ad3951516c170efae3de2b543f0d40be
Oskar Niburski <oskarniburski@gmail.com>
2015-12-29 23:08:19 -0500

    Reusuable input component

--- react-node/src/components/authors/authorForm.js
+++ react-node/src/components/authors/authorForm.js
@@ -1,13 +1,14 @@
 "use strict";
 
 var React = require('react');
+var Input = require('../common/textInput')
 // smart level, passing data down to dumb data
 var AuthorForm = React.createClass({
   render: function() {
     return (
       <form>
-        <input type="text" name="firstName" placeholder="firstname" ref="firstName" value={this.props.author.firstName} onChange={this.props.onChange} /><br />
-        <input type="text"  placeholder="lastname" name="lastName" ref="lastName" value={this.props.author.lastName} onChange={this.props.onChange} /><br />
+        <Input name="firstName" label="First Name" value={this.props.author.firstName} onChange={this.props.onChange} /><br />
+        <Input name="lastName" label="Last Name" value={this.props.author.lastName} onChange={this.props.onChange} />
         <br />
         <input type="submit" value="save" />
       </form>
