6c73102b609ff9201786db9811cbfa20db55eb69
Oskar Niburski <oskarniburski@gmail.com>
2016-01-10 14:23:53 -0500

    Start of DAO file

---
+++ java-webdev/BookDAO.java
@@ -0,0 +1,13 @@
+package com.apress.books.dao;
+import com.apress.books.model.Book;
+import com.apress.books.model.Category;
+import java.util.List;
+
+public interface BookDAO {
+  public List<Book>findAllBooks();
+  public List<Book>searchBooksByKeyword(String keyWord);
+  public List<Category>findAllCategories();
+  public void insert(Book book);
+  public void update(Book book);
+  public void delete(Long bookId);
+}
