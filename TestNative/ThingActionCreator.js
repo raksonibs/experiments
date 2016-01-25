88b93eba0f437180f535d9b7c0c7121e6009b793
Oskar Niburski <oskarniburski@gmail.com>
2016-01-24 19:27:27 -0500

    Other shared files

---
+++ TestNative/ThingActionCreator.js
@@ -0,0 +1,34 @@
+var dispatcher = require('./../dispatcher/appDispatcher.js');
+
+module.exports = {
+  add:function(thing) {
+    dispatcher.dispatch({
+      payload: thing,
+      type:"thing:add"
+    })
+  },
+  love: function(thing) {
+    dispatcher.dispatch({
+      payload: thing,
+      type:"thing:love"
+    })
+  },
+  unlove: function(thing) {
+    dispatcher.dispatch({
+      payload: thing,
+      type:"thing:unlove"
+    })
+  },
+  update: function(thing) {
+    dispatcher.dispatch({
+      payload: thing,
+      type:"thing:update"
+    })
+  },
+  delete:function(thing) {
+    dispatcher.dispatch({
+      payload: thing,
+      type:"thing:delete"
+    })
+  }
+}
---
+++ TestNative/ThingStore.js
@@ -0,0 +1,122 @@
+var dispatcher = require('./../dispatcher/appDispatcher.js');
+var helper = require('./../helpers/APIHelper.js');
+import _ from 'lodash';
+
+function ThingStore() {
+  var things = []
+  var listeners = []
+
+  function loadThings() {
+    helper.get("api/things")
+    .then(function(data) {
+      things = data;
+      triggerListeners();
+      return things
+    })
+  }
+
+  function getThings() {
+    return things;
+  }
+
+  function onChange(listener) {
+    listeners.push(listener);
+  }
+
+  function deleteThing(thing) {
+    var index 
+    things.filter(function(_thing, _index){
+      if (_thing.name == thing.name) {
+        index = _index
+      }
+    })
+
+    things.splice(index, 1);
+    triggerListeners();
+
+    // confirmation before the server 
+
+    helper.delete("api/things/" + thing._id);
+
+  }
+
+  function addThing(thing) {
+    things.push(thing)
+    triggerListeners()
+
+    helper.post('api/things', thing)
+      .then(function(data) {
+      console.log(data)      
+    })
+  }
+
+  function setThingTruth(thing, isLoved) {
+    var _thing = things.filter(function(a) {return a.name == thing.name})[0]
+    console.log(thing)
+    console.log(thing.loved)
+    if (thing.loved === 'true') {        
+      thing.loved = false 
+    } else {
+      thing.loved = true
+    }
+    console.log(thing.loved)
+
+    triggerListeners()
+
+    helper.patch("api/things/", thing._id, thing);
+  }
+
+  function changeThing(thing) {
+    var _thing = things.filter(function(a) {return a.name == thing.name})[0]
+    thing.loved = isLoved || false;
+  }
+
+  function triggerListeners() {
+    listeners.forEach(function(listener) {
+      listener(things)
+    })
+  }
+
+  function login(user) {
+    this.setState({user: user});
+  }
+
+  loadThings()
+
+  dispatcher.register(function(event) {
+    var split = event.type.split(':');
+    if (split[0] == 'thing') {
+      switch(split[1]) {
+        case "add":
+          addThing(event.payload);
+          break;
+        case "delete":
+          deleteThing(event.payload);
+          break;
+        case "love":
+          setThingTruth(event.payload, true);
+          break;
+        case "unlove":
+          setThingTruth(event.payload, false);
+          break;
+        case "update":
+          changeThing(event.payload, false);
+          break;
+      }
+    } else {
+      switch(split[1]) {
+        case "login":
+          login(event.payload);
+          break;
+      }
+    }
+  })
+
+  return {
+    getThings: getThings,
+    onChange: onChange,
+    loadThings: loadThings
+  }
+}
+
+module.exports = new ThingStore();
