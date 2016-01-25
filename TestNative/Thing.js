b3f956728c92c987f7ed1aa31edf6d3a3ef55a2e
Oskar Niburski <oskarniburski@gmail.com>
2016-01-25 00:13:57 -0500

    File changes

---
+++ TestNative/Thing.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import RaisedButton from 'material-ui/lib/raised-button';
+import Card from 'material-ui/lib/card/card';
+import CardText from 'material-ui/lib/card/card-text';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardActions from 'material-ui/lib/card/card-actions';
+import {RouteHandler} from 'react-router';
+
+module.exports = React.createClass({
+  makeLoved: function(e) {
+    e.preventDefault()
+    this.props.update(this.props.thing)
+  },
+  delete: function(e) {
+    e.preventDefault()
+    this.props.delete(this.props.thing)
+  },
+  render: function() {
+        return (
+          <Card className="list-item">
+            <CardHeader
+              title={this.props.thing.name}
+              subtitle={this.props.thing._id} />
+            <CardText>
+               Could be loved. Right now it is {this.props.thing.loved}
+            </CardText>
+            <CardActions>
+              <RaisedButton onClick={this.makeLoved} label="Make Un/Loved" />
+              <RaisedButton primary={true} onClick={this.delete} label="Delete" />
+            </CardActions>
+          </Card>
+        );
+    }
+})
