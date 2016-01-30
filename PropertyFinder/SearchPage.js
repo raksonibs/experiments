37b80cc1e310ee4de1e27fc743089d22477f15a4
Oskar Niburski <oskarniburski@gmail.com>
2016-01-30 15:36:18 -0500

    Search results

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -12,6 +12,8 @@ var {
   Component
 } = React;
 
+var SearchResults = require('./SearchResults');
+
 function urlForQueryAndPage(key, value, pageNumber) {
   var data = {
       country: 'uk',
@@ -49,7 +51,11 @@ class SearchPage extends Component {
   _handleResponse(response) {
     this.setState({ isLoading: false , message: '' });
     if (response.application_response_code.substr(0, 1) === '1') {
-      console.log('Properties found: ' + response.listings.length);
+      this.props.navigator.push({
+        title: 'Results',
+        component: SearchResults,
+        passProps: {listings: response.listings}
+      });
     } else {
       this.setState({ message: 'Location not recognized; please try again.'});
     }
