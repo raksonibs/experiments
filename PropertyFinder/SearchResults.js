1059182c69771f1d87d96df1f63da1a0ac5f3150
Oskar Niburski <oskarniburski@gmail.com>
2016-01-30 15:39:24 -0500

    Property view

--- PropertyFinder/SearchResults.js
+++ PropertyFinder/SearchResults.js
@@ -11,6 +11,8 @@ var {
   Component
 } = React;
 
+var PropertyView = require('./PropertyView');
+
 var styles = StyleSheet.create({
   thumb: {
     width: 80,
@@ -54,6 +56,12 @@ class SearchResults extends Component {
 
   rowPressed(propertyGuid) {
     var property = this.props.listings.filter(prop => prop.guid === propertyGuid)[0];
+
+    this.props.navigator.push({
+      title: "Property",
+      component: PropertyView,
+      passProps: {property: property}
+    });
   }
  
   renderRow(rowData, sectionID, rowID) {
