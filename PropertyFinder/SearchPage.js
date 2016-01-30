1e338dd19e2417fe770d9b9caa94877b10caa486
Oskar Niburski <oskarniburski@gmail.com>
2016-01-30 15:44:41 -0500

    Location services

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -78,6 +78,22 @@ class SearchPage extends Component {
     var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
     this._executeQuery(query);
   }
+
+  onLocationPressed() {
+    navigator.geolocation.getCurrentPosition(
+      location => {
+        var search = location.coords.latitude + ',' + location.coords.longitude;
+        this.setState({ searchString: search });
+        var query = urlForQueryAndPage('centre_point', search, 1);
+        this._executeQuery(query);
+      },
+      error => {
+        this.setState({
+          message: 'There was a problem with obtaining your location: ' + error
+        });
+      });
+  }
+
   render() {
     var spinner = this.state.isLoading ?
       ( <ActivityIndicatorIOS
@@ -106,8 +122,11 @@ class SearchPage extends Component {
             <Text style={styles.buttonText}>Go</Text>
           </TouchableHighlight>
         </View>
-        <TouchableHighlight style={styles.button}
-        underlayColor='#99d9f4'>
+        <TouchableHighlight 
+          style={styles.button}
+          underlayColor='#99d9f4'
+          onPress={this.onLocationPressed.bind(this)}
+        >
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
         <Image source={require('image!house')} style={styles.image}/>
