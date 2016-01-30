e7644c7234803bdffd3c2ae1a9b2403822824d4c
Oskar Niburski <oskarniburski@gmail.com>
2016-01-30 15:18:18 -0500

    Search page

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -16,20 +16,28 @@ var {
 class SearchPage extends Component {
   render() {
     return (
-      <View style={styles.flowRight}>
-        <TextInput
-        style={styles.searchInput}
-        placeholder='Search via name or postcode'/>
-        <TouchableHighlight style={styles.button}
-          underlayColor='#99d9f4'>
-        <Text style={styles.buttonText}>Go</Text>
-        </TouchableHighlight>
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Search for houses to buy!
+        </Text>
+        <Text style={styles.description}>
+          Search by place-name, postcode or search near your location.
+        </Text>
+        <View style={styles.flowRight}>
+          <TextInput
+          style={styles.searchInput}
+          placeholder='Search via name or postcode'/>
+          <TouchableHighlight style={styles.button}
+            underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
         </View>
         <TouchableHighlight style={styles.button}
         underlayColor='#99d9f4'>
-        <Text style={styles.buttonText}>Location</Text>
+          <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
-      </View
+        <Image source={require('image!house')} style={styles.image}/>
+      </View>
     );
   }
 }
@@ -45,6 +53,43 @@ var styles = StyleSheet.create({
     padding: 30,
     marginTop: 65,
     alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
+  },
+  image: {
+    width: 217,
+    height: 138
   }
 });
 
