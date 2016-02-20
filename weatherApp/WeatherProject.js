ff41d109f46fec5f4ce65570035237aed83affc0
Oskar Niburski <oskarniburski@gmail.com>
2016-02-20 13:25:20 -0500

    Full size image not working

--- weatherApp/WeatherProject.js
+++ weatherApp/WeatherProject.js
@@ -6,11 +6,17 @@ var React = require('react-native'); var {
   Image
 } = React;
 
+var Forecast = require('./Forecast');
 
 var WeatherProject = React.createClass({
   getInitialState() {
     return ({ 
-      zip: ''
+      zip: '',
+      forecast: {
+        main: 'Clouds',
+        description: 'few clouds',
+        temp: 45.7
+      }
     });
   },
 
@@ -22,29 +28,74 @@ var WeatherProject = React.createClass({
 
   render() { 
     return (
-      <View style={styles.container}> 
-        <Text style={styles.welcome}> You input {this.state.zip}.</Text>
-        <TextInput
-        style={styles.input}
-        onSubmitEditing={this._handleTextChange}/> 
-      </View>
-      ); 
-    }
+      <View style={styles.container}>
+         <Image source={require('image!flowers')}
+             resizeMode='cover'
+             style={styles.backdrop}>
+           <View style={styles.overlay}>
+             <View style={styles.row}>
+               <Text style={styles.mainText}>
+                   Current weather for 
+                </Text>
+               <View style={styles.zipContainer}>
+                 <TextInput
+                  style={[styles.zipCode, styles.mainText]} 
+                  returnKeyType='go' 
+                  onSubmitEditing={this._handleTextChange}/>
+               </View>
+             </View>
+          <Forecast
+            main={this.state.forecast.main} 
+            description={this.state.forecast.description} 
+            temp={this.state.forecast.temp}/>
+        </View>
+      </Image>
+    <View>
+    ); 
+  }
 });
 
-var styles = StyleSheet.create({ container: {
+var baseFontSize = 16
+
+var styles = StyleSheet.create({ 
+  container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-}, welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-}, input: {
-    fontSize: 20,
-    borderWidth: 2,
-    height: 40
-    }
+    paddingTop: 30
+  },
+  backdrop: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: '#000000',
+    opacity: 0.5,
+    flexDirection: 'column',
+    alignItems: 'center'
+  }, row: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'flex-start',
+    padding: 30
+  },
+  zipContainer: {
+    flex: 1,
+    borderBottomColor: '#DDDDDD',
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3
+  }, 
+  zipCode: {
+    width: 50,
+    height: baseFontSize,
+  },
+  mainText: {
+    flex: 1,
+    fontSize: baseFontSize,
+    color: '#FFFFFF'
+  }
 });
+
 module.exports = WeatherProject;
--- weatherApp/ios/weatherApp/AppDelegate.m
+++ weatherApp/ios/weatherApp/AppDelegate.m
@@ -42,7 +42,7 @@
 //   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"weatherApp"
+                                                      moduleName:@"WeatherProject"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
 
