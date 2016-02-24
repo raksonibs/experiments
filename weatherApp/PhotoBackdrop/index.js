86092526735379a9fc84a6bf08a5cf1523ffaf1d
Oskar Niburski <oskarniburski@gmail.com>
2016-02-24 18:43:53 -0500

    Camera components with selection using ImagePicker

---
+++ weatherApp/PhotoBackdrop/index.js
@@ -0,0 +1,28 @@
+var React = require('react-native'); var {
+Image,
+ImagePickerIOS
+} = React;
+var styles = require('./style.js');
+var Button = require('./../Button');
+var PhotoBackdrop = React.createClass({ getInitialState() {
+return {
+photoSource: require('./img/flowers.jpg')
+} },
+  _pickImage() {
+    ImagePickerIOS.openCameraDialog(
+      {},
+      (data) => {
+this.setState({ photoSource: {uri: data}
+}); },
+()=>{
+console.log('User canceled the action');
+}); },
+render() { return ( <Image
+style={styles.backdrop}
+source={ this.state.photoSource } resizeMode='cover'> {this.props.children}
+<Button
+style={styles.button} label="Load Image" onPress={this._pickImage}/>
+</Image>
+); }
+});
+module.exports = PhotoBackdrop;
