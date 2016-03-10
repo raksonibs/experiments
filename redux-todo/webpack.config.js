a2dabd6b850af0f00af68cf983595c13cda54ca0
Oskar Niburski <oskarniburski@gmail.com>
2016-03-10 00:30:41 -0500

    Broken setup con counter?

--- redux-todo/webpack.config.js
+++ redux-todo/webpack.config.js
@@ -1,14 +1,30 @@
- var path = require('path');
-    module.exports = {
-        entry: './es6/state.js',
-        output: {
-            path: __dirname,
-            filename: 'bundle.js'
-        },
-        module: {
-            loaders: [
-                { test: path.join(__dirname, 'es6'),
-                  loader: 'babel-loader' }
-            ]
-        }
-    };
+
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        exclude: /node_modules/,
+        include: __dirname
+      }
+    ]
+  }
+}
