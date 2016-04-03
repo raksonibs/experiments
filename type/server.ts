491a58c00d4f78a273d5b3664a1839902ad5ffcd
Oskar Niburski <oskarniburski@gmail.com>
2016-04-03 18:33:06 -0400

    Cluster example with worker and masters

--- type/server.ts
+++ type/server.ts
@@ -1,24 +1,33 @@
 /// <reference path="typings/tsd.d.ts" />
-// file created bt tsd references typs for all libraries.
 
+import * as cluster from 'cluster';
+import { cpus } from 'os';
 import * as Hapi from 'hapi';
 
-const server = new Hapi.Server();
-server.connection({
-  host: 'localhost',
-  port: 8000
-})
+const numCPUs = cpus().length;
 
-server.route({
-  method: 'GET',
-  path: '/hello/{name}',
-  handler: function(request, reply) {
-
-    return reply('hello ' + request.params['name']);
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-});
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  const server = new Hapi.Server();
+  server.connection({
+    host: 'localhost',
+    port: 8000
+  });
 
-server.start((err) => {
-  if (err) { throw err; }
-  console.log('Server running at:', server.info.uri);
-});
+  server.route({
+    method: 'GET',
+    path: '/hello/{name}',
+    handler: (request, reply) => reply('hello ' + request.params['name'])
+  });
+
+  server.start((err) => {
+    if (err) { throw err; }
+    console.log('Server running at:', server.info.uri);
+  });
+}
