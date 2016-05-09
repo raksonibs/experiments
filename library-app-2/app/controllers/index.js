a051773a67a905a794eb39732c925d2e52f1973b
Oskar Niburski <oskarniburski@gmail.com>
2016-05-08 21:24:24 -0400

    Admin, also invitations

--- library-app-2/app/controllers/index.js
+++ library-app-2/app/controllers/index.js
@@ -1,26 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  textMessage: '',
+
+  headerMessage: 'Coming Soon',
+  responseMessage: '',
   emailAddress: '',
+
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  isDisabled:  Ember.computed.not('isValid'),
-  actualEmailAddress: Ember.computed('emailAddress', function() {
-    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
-  }),
-  emailAddressChanged: Ember.observer('emailAddress', function() { 
-    console.log('observer is called', this.get('emailAddress')); 
-  }),
+  isDisabled: Ember.computed.not('isValid'),
+
   actions: {
+
     saveInvitation() {
-      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
-    }, 
-    sendMessage: function() {
-      this.set('responseMessage', 'Yo'+ this.get('emailAddress'));
-      this.set('emailAddress', '');
-      this.set('textMessage', '');
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('invitation', {
+        email: email
+      });
+
+      newInvitation.save().then((response) => {
+        this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
+        this.set('emailAddress', '');
+      });
+
     }
   }
+
 });
