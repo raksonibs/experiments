0cc8042c06b79fadb0dc08603e72d316d81b1088
Oskar Niburski <oskarniburski@gmail.com>
2016-05-10 00:08:43 -0400

    Admin seeder

---
+++ library-app-2/app/models/book.js
@@ -0,0 +1,10 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+
+export default Model.extend({
+  title: attr('string'),
+  releaseYear: attr('date'),
+  library: belongsTo('library'),
+  author: belongsTo('author')
+});
--- library-app-2/app/models/library.js
+++ library-app-2/app/models/library.js
@@ -1,12 +1,12 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-
+import { hasMany } from 'ember-data/relationships'
 import Ember from 'ember';
 
 export default Model.extend({
   name: attr('string'),
   address: attr('string'),
   phone: attr('string'),
-
+  books: hasMany('books'),
   isValid: Ember.computed.notEmpty('name')
 });
